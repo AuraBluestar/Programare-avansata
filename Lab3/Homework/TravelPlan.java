beb3431bcced5db66a7d4f803b82e27539bf27a8
Patricia Onisor <77615799+AuraBluestar@users.noreply.github.com>
2024-03-19 13:46:20 +0200

    Update TravelPlan.java

--- Lab3/Homework/TravelPlan.java
+++ Lab3/Homework/TravelPlan.java
@@ -7,7 +7,6 @@ import java.util.Map;
 public class TravelPlan {
 
     private final Map<Location, LocalDate> plan;
-    
 
     public TravelPlan() {
         this.plan = new HashMap<>();
@@ -15,18 +14,28 @@ public class TravelPlan {
 
     public void addVisit(Location location, LocalDate date) {
         plan.put(location, date);
+        
     }
 
-    /*public void addVisit(Attraction attraction, LocalDate date) {
-        plan.put(location, date);
-    }*/
-    
     public void displayTravelPlan() {
-        System.out.println("Travel Plan:");
+        System.out.println("Travel Plan: ");
+        //System.out.println(" ");
+        int day = 1;
+        LocalDate data = LocalDate.of(2000, 1, 1);
         for (Map.Entry<Location, LocalDate> entry : plan.entrySet()) {
             Location location = entry.getKey();
             LocalDate date = entry.getValue();
-            System.out.println("Date: " + date + ", Location: " + location.getName());
+            if (data == date) {
+                System.out.println("Location: " + location.getName());
+            } else {
+                System.out.println("Day " + day + ", Date: " + date);
+                System.out.println("Location: " + location.getName());
+                data = date;
+                day++;
+            }
+
         }
+        System.out.println(" ");
     }
+    
 }
