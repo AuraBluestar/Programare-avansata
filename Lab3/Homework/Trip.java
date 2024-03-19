f29deba0824bb4f7fa406d9064b388a2d5987c73
Patricia Onisor <77615799+AuraBluestar@users.noreply.github.com>
2024-03-19 13:46:48 +0200

    Update Trip.java

--- Lab3/Homework/Trip.java
+++ Lab3/Homework/Trip.java
@@ -1,10 +1,9 @@
-
 package lab3.lab3;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
-
 
 public class Trip {
 
@@ -18,14 +17,14 @@ public class Trip {
         this.beginTrip = beginTrip;
         this.endTrip = endTrip;
     }
-    
+
     public void addAttraction(Location attraction) {
         attractions.add(attraction);
     }
-    
-    public void getAttractions(LocalDate date){
+
+    public void getAttractions(LocalDate date) {
         List<Location> visitableNotPayable = new ArrayList<>();
-        
+
         // Iterate through attractions
         for (Location attraction : attractions) {
             if (attraction.isVisitable(date) && !attraction.isPayable()) {
@@ -33,12 +32,23 @@ public class Trip {
             }
         }
         // Sort attractions by opening hour
-        Collections.sort(visitableNotPayable , (loc1, loc2) -> loc1.getOpeningHour(beginTrip).compareTo(loc2.getOpeningHour(beginTrip)));
-        
+        Collections.sort(visitableNotPayable, (loc1, loc2) -> loc1.getOpeningHour(beginTrip).compareTo(loc2.getOpeningHour(beginTrip)));
+
         // Display sorted attractions
         System.out.println("Locations that are visitable and not payable, sorted by opening hour:");
         for (Location location : attractions) {
             System.out.println(location.getName() + " - Opening hour: " + location.getOpeningHour(date));
         }
+    }
+    public String getCity() {
+        return city;
+    }
+
+    public LocalDate getBeginTrip() {
+        return beginTrip;
+    }
+
+    public LocalDate getEndTrip() {
+        return endTrip;
     }
 }
